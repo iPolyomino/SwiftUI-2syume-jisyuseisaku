5824854b85f2bc414c10d069f583e5ea5e0a9b14
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-05-29 15:45:59 +0900

    Initial Commit

---
+++ SwiftUI-2syume jisyuseisaku/SwiftUI_2syume_jisyuseisakuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_2syume_jisyuseisakuApp.swift
+//  SwiftUI-2syume jisyuseisaku
+//
+//  Created by Hiroaki Ishiga on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_2syume_jisyuseisakuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
