3f8311bd83c7de5ddf8353fd0dc6356852450748
lioisshi <84262564+lioisshi@users.noreply.github.com>
2021-05-30 20:06:15 +0900

    今日の晩ご飯決めてくれるアプリ（改良中）

--- SwiftUI-2syume jisyuseisaku/ContentView.swift
+++ SwiftUI-2syume jisyuseisaku/ContentView.swift
@@ -8,10 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+ let one = ["肉","魚","麺類"]
+ let two =  ["外食","自炊","出前"]
+ let three = ["500円以内","1000円以内","1000円以上！"]
+    
+ @State var te = ""
+ @State var ta = ""
+ @State var tt = ""
+    //配列　p45
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack{
+            Button("メニューは？"){
+                te = one.randomElement()!
+            }
+            .foregroundColor(.white)
+            .background(
+            Capsule()
+                .foregroundColor(.blue)
+                .frame(width: 160, height : 40)
+            )
+            Text(te)
+                .font(.largeTitle)
+                .padding()
+            Button("どこで食べる?"){
+                ta = two.randomElement()!
+            }
+            .foregroundColor(.white)
+            .background(
+            Capsule()
+                .foregroundColor(.blue)
+                .frame(width: 160, height : 40)
+            )
+            Text(ta)
+                .font(.largeTitle)
+                .padding()
+            Button("予算は？"){
+                tt = three.randomElement()!
+            }
+            .foregroundColor(.white)
+            .background(
+            Capsule()
+                .foregroundColor(.blue)
+                .frame(width: 160, height : 40)
+            )
+            Text(tt)
+                .font(.largeTitle)
+                .padding()
+            }
+        }
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -19,3 +64,6 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
