d1850a1757fa315486e4497d595049f4bf7c27a1
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2021-06-05 15:26:59 +0900

    add images

--- SwiftUI-2syume jisyuseisaku/ContentView.swift
+++ SwiftUI-2syume jisyuseisaku/ContentView.swift
@@ -8,19 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
- let one = ["肉","魚","麺類","ご飯もの"]
+ let menu = ["肉","魚","麺類","ご飯もの"]
+ let images = ["niku", "sakana", "men", "teishoku"]
  let two =  ["外食","自炊","出前"]
  let three = ["500円以内","1000円以内","1000円以上！"]
     
- @State var te = ""
- @State var ta = ""
- @State var tt = ""
+ @State var te = " "
+ @State var imageIndex = 0
+ @State var ta = " "
+ @State var tt = " "
     //配列　p45
     
     var body: some View {
         VStack{
             Button("メニューは？"){
-                te = one.randomElement()!
+                let menuIndex = Int.random(in: 0..<menu.count)
+                te = menu[menuIndex]
+                imageIndex = menuIndex
             }
             .foregroundColor(.white)
             .background(
@@ -56,6 +60,10 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .padding()
             }
+            Image(images[imageIndex])
+                .resizable()
+                .frame(width: 300, height: 300)
+                .aspectRatio(contentMode:.fill)
         }
 }
 
